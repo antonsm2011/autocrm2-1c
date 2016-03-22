c0013999e0fd4b24b25c0d5d6ba0e56d9a0895ca
Anton Tyutin <anton@tyutin.ru>
2016-03-22 12:12:56 +0300

    Переделка логгирования с файлов на БД.

--- services/logger.php
+++ services/logger.php
@@ -1,8 +1,7 @@
 <?php
 /** @var \Silex\Application $app */
 
-use Monolog\Formatter\LineFormatter;
-use Monolog\Handler\HandlerInterface;
+use Monolog\Handler\AbstractProcessingHandler;
 use Monolog\Logger;
 use Silex\Provider\MonologServiceProvider;
 
@@ -18,8 +17,64 @@ $app->extend('monolog', function (Logger $service) use ($app) {
     });
 });
 
-$app->extend('monolog.handler', function (HandlerInterface $service) use ($app) {
-    return $service->setFormatter(
-        new LineFormatter("%datetime% [%process_id%] %channel%.%level_name%: %message% %context% %extra%\n")
+$app['monolog.handler'] = function () use ($app) {
+    return new PDOHandler(
+        $app['db'],
+        MonologServiceProvider::translateLevel($app['monolog.level']),
+        $app['monolog.bubble']
     );
-});
+};
+
+class PDOHandler extends AbstractProcessingHandler
+{
+    private $initialized = false;
+    /** @var PDO */
+    private $pdo;
+    /** @var PDOStatement */
+    private $statement;
+
+    public function __construct(PDO $pdo, $level = Logger::DEBUG, $bubble = true)
+    {
+        $this->pdo = $pdo;
+        parent::__construct($level, $bubble);
+    }
+
+    protected function write(array $record)
+    {
+        if (!$this->initialized) {
+            $this->initialize();
+        }
+
+        $this->statement->execute(array(
+            'time' => $record['datetime']->format('Y-m-d H:i:s'),
+            'process_id' => $record['process_id'],
+            'channel' => $record['channel'],
+            'level' => Logger::getLevelName($record['level']),
+            'message' => $record['message'],
+            'context' => json_encode($record['context'], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT),
+            'extra' => json_encode($record['extra'], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT),
+        ));
+    }
+
+    private function initialize()
+    {
+        $this->pdo->exec(<<<'SQL'
+            CREATE TABLE IF NOT EXISTS logs (
+                time DATETIME,
+                process_id CHAR(32),
+                channel VARCHAR(255),
+                level VARCHAR(15),
+                message TEXT,
+                context LONGTEXT,
+                extra LONGTEXT
+            )
+SQL
+        );
+        $this->statement = $this->pdo->prepare(
+            'INSERT INTO logs (time, process_id, channel, level, message, context, extra) '
+                . 'VALUES (:time, :process_id, :channel, :level, :message, :context, :extra)'
+        );
+
+        $this->initialized = true;
+    }
+}
